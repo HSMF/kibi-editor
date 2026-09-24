2a5ba59c3f208366c22459176dc519ae7cce638b
Conradin Laux <conradin.laux@gmail.com>
2026-09-24 10:05:10 +0200

    write crash report on crash because stderr is failing me

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 #![cfg_attr(not(test), warn(clippy::dbg_macro))]
 
-use std::{env, fmt::Display, io::Write, ops::ControlFlow, os::fd::AsRawFd};
+use std::{
+    env, fmt::Display, io::Write, ops::ControlFlow, os::fd::AsRawFd, panic, time::SystemTime,
+};
 
 use anyhow::anyhow;
 use termios::Termios;
@@ -261,6 +263,22 @@ fn draw_rows(conf: &mut EditorConfig) {
 }
 
 fn main() -> anyhow::Result<()> {
+    panic::set_hook(Box::new(move |info| {
+        let backtrace = std::backtrace::Backtrace::force_capture();
+        let now = SystemTime::now()
+            .duration_since(SystemTime::UNIX_EPOCH)
+            .unwrap()
+            .as_secs();
+        let mut f = std::fs::File::create(format!("./kibi-crash-{now}")).unwrap();
+        if let Some(payload) = info.payload_as_str() {
+            let _ = writeln!(f, "payload: {payload}");
+        }
+        if let Some(loc) = info.location() {
+            let _ = writeln!(f, "panic at {loc}");
+        }
+
+        let _ = writeln!(f, "backtrace {:#?}", backtrace);
+    }));
     let options = options::Options::parse(env::args());
 
     if options.print_supported_keymaps {
@@ -280,6 +298,7 @@ fn main() -> anyhow::Result<()> {
     }
 
     let orig_termios = enter_raw_mode();
+
     let _alternate_screen = AlternateScreen::new();
     let mut conf = EditorConfig::init()?;
 
